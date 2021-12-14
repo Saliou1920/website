b8ca142a53476b49bfdbf59a394e02256167af3f
Saliou1920 <diopsaliou45@gmail.com>
2021-12-14 14:35:02 -0400

    adding conpany logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Experience from "./components/Experience/Experience";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Experience />
     </div>
   );
 }
---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./experience.css";
+export default function Experience() {
+  return (
+    <div className="experience">
+      <div className="experience-header">
+        <div className="experience-header-title">
+          <img src="../../greystone.jpeg" alt="greystone" />
+          <span>Greystone Energy</span>
+        </div>
+        <div className="experience-header-title">
+          <img src="../../umoncton.png" alt="umoncton" />
+          <span>Universite de Moncton</span>
+        </div>
+        <div className="experience-header-title">
+          <img src="../../prime.jpeg" alt="prime" />
+          <span>Prime</span>
+        </div>
+      </div>
+    </div>
+  );
+}
