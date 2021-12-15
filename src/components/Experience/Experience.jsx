2caf47de9d6b6e191255b51c77f6c9d47620d01f
Saliou1920 <diopsaliou45@gmail.com>
2021-12-14 20:46:14 -0400

    styling experience component

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import "./experience.css";
+import { Container } from "react-bootstrap";
+
 export default function Experience() {
   return (
     <div className="experience">
-      <div className="experience-header">
-        <div className="experience-header-title">
-          <img src="../../greystone.jpeg" alt="greystone" />
-          <span>Greystone Energy</span>
+      <Container>
+        <div className="experience-header">
+          <div className="experience-header-title">
+            <img src="../../greystone.jpeg" alt="greystone" />
+            <span>Greystone Energy</span>
+          </div>
+          <div className="experience-header-title">
+            <img src="../../umoncton.png" alt="umoncton" />
+            <span>Universite de Moncton</span>
+          </div>
+          <div className="experience-header-title">
+            <img src="../../prime.jpeg" alt="prime" />
+            <span>Prime</span>
+          </div>
         </div>
-        <div className="experience-header-title">
-          <img src="../../umoncton.png" alt="umoncton" />
-          <span>Universite de Moncton</span>
-        </div>
-        <div className="experience-header-title">
-          <img src="../../prime.jpeg" alt="prime" />
-          <span>Prime</span>
-        </div>
-      </div>
 
-      <div className="experience-body">
-        <div className="experience-body-title"></div>
-      </div>
+        <div className="experience-body">
+          <div className="experience-body-title"></div>
+        </div>
+      </Container>
     </div>
   );
 }
