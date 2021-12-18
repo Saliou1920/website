7c48872d3085a889088afed1052d666b28870a40
Saliou1920 <diopsaliou45@gmail.com>
2021-12-18 19:29:47 -0400

    adding projects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Experience from "./components/Experience/Experience";
 import Header from "./components/Header/Header";
+import Porjects from "./components/Projects/Porjects";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Experience />
+      <Porjects />
     </div>
   );
 }
