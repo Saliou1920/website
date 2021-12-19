db082a34107cd71cd762aa11492c68e507a44dec
Saliou1920 <diopsaliou45@gmail.com>
2021-12-18 21:46:01 -0400

    ading AOS animation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import Experience from "./components/Experience/Experience";
 import Header from "./components/Header/Header";
 import Porjects from "./components/Projects/Porjects";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
   return (
     <div className="App">
       <Header />
