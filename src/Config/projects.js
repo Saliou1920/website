a1c7bc2c3e6fe6c44d3ea5575e883f41d7de72f8
Saliou1920 <diopsaliou45@gmail.com>
2022-02-25 00:58:18 -0400

    :art: project lists

---
+++ src/Config/projects.js
@@ -0,0 +1,31 @@
+const projects = [
+  {
+    name: "Images Gallery",
+    description:
+      "A web app that uses the Unplash API to display beautiful images. The user can search for images by keyword, and can also save the images to their own collection. The user can also view their saved images.",
+    languages: "React, React-Router, Unplash API, firebase auth",
+    link: "https://github.com/Saliou1920/Images-Gallery",
+    image: "gallery.png",
+    animation: "fade-up-right",
+  },
+  {
+    name: "Word Scrambler",
+    description:
+      "A word scrambler and typing game. The user has to type the unscrambled word. If the whole sentence is correctly guessed, there should be a next button that shows at the bottom. If you click it would try to fetch the next sentence from the API",
+    languages: "React, HTML, CSS, Javascript",
+    link: "https://github.com/Saliou1920/word_scrambler",
+    image: "word.png",
+    animation: "zoom-in",
+  },
+  {
+    name: "BLOG",
+    description:
+      " A blog website that uses MYSQL to store and retrieve data. The admin can create a new post, edit an existing post, and delete a post. He can also create a new user, edit an existing user, and delete a user.",
+    link: "https://github.com/Saliou1920/Blog",
+    languages: "PHP, HTML, CSS, Javascript, MYSQL, CodeIgniter",
+    image: "blog.png",
+    animation: "fade-up-left",
+  },
+];
+
+export default projects;
