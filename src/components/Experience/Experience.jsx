30d4f432580d5a1c23cadecb3f169c206d60a7ed
Saliou1920 <diopsaliou45@gmail.com>
2022-02-26 13:17:24 -0400

    :star: refactoring

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./experience.css";
 import { Col, Container, Row } from "react-bootstrap";
-
+import experience from "./experience.js";
 export default function Experience() {
   return (
     <div className="experience" id="experience">
@@ -29,67 +29,16 @@ export default function Experience() {
 
           <div className="experience-body-content">
             <Row>
-              <Col xs={12} md={4} className="experience-body-title">
-                <div />
-                <h4>Software Developer intern</h4>
-                <small>Greystone Energy Inc, Moncton</small>
-                <div className="experience-body-description">
-                  <p>
-                    Worked with the operation manager to create dashboard using
-                    JavaScript, SQL, and Power Bi to evaluate employees
-                    performance using the company large Oracle database.
-                  </p>
-                  <p>
-                    Built reports for the operation manager to understand the
-                    data analysis steps, enabling him to take important
-                    decisions based on performance.
-                  </p>
-                </div>
-              </Col>
-
-              <Col xs={12} md={4} className="experience-body-title">
-                <div />
-                <h4>AI Research Assistant</h4>
-                <small>
-                  Perception, Robotics and intelligent Machines (PRIME), U
-                  Moncton
-                </small>
-                <div className="experience-body-description">
-                  <p>
-                    Worked with Professor Akhloufi and MSc students to create a
-                    simulation tool for forest fires to help prevent
-                    propagation.
-                  </p>
-                  <p>
-                    Produced a 3D photorealistic model that can predict
-                    effectively fire propagation from parameters such as wind
-                    speed, humidity etc...
-                  </p>
-                  <p>
-                    Used the Unity game engine, C# programming language and
-                    state of the art deep learning techniques to build the
-                    model.
-                  </p>
-                </div>
-              </Col>
-
-              <Col xs={12} md={4} className="experience-body-title">
-                <div />
-                <h4>C++ programming Tutor</h4>
-                <small>Université de Moncton, Moncton</small>
-                <div className="experience-body-description">
-                  <p>
-                    Worked closely with the professor and a few other computer
-                    science students to assist junior and sophomore students
-                    with C++ programming.
-                  </p>
-                  <p>
-                    Successfully, analyzed and debugged code written by various
-                    students and clarified the new concepts introduced in the
-                    course.
-                  </p>
-                </div>
-              </Col>
+              {experience.map((experience, index) => (
+                <Col xs={12} md={6} lg={4} className="experience-body-title">
+                  <div />
+                  <h4>{experience.title}</h4>
+                  <small>{experience.company}</small>
+                  <div className="experience-body-description">
+                    <p>{experience.description}</p>
+                  </div>
+                </Col>
+              ))}
             </Row>
           </div>
         </div>
---
+++ src/components/Experience/experience.js
@@ -0,0 +1,34 @@
+const experience = [
+  {
+    title: "Software Developer",
+    company: "Jarvis Consulting, Toronto",
+    description:
+      "Developed an MVP for a Linux Cluster Monitoring agent using Agile techniques, a virtual machine provisioned using Google Cloud Platform, bash scripts, git, Docker, and PostgreSQL. Developed apps in Java to mimic Linux grep command and connect to PostgreSQL database. Performed unit testing using the JUnit library. Participated in daily scrum meetings and weekly code reviews",
+  },
+  {
+    title: "Software Developer Intern",
+    company: "Greystone Energy Inc, Moncton",
+    description:
+      "Worked with the operation manager to create dashboards using Javascript, SQL, and Power Bi to evaluate employees' performance. Built reports for the operation manager to understand the data analysis steps, enabling him to take important decisions based on performance.",
+  },
+  {
+    title: "C++ programming Tutor",
+    company: "Université de Moncton, Moncton",
+    description:
+      "Worked closely with the professor and a few other computer science students to assist junior and sophomore students with C++ programming. Successfully, analyzed and debugged code written by various students and clarified the new concepts introduced in the course.",
+  },
+  {
+    title: "AI Research Assistant",
+    company: "Perception, Robotics and intelligent Machines (PRIME), Moncton",
+    description:
+      "Worked with Professor Akhloufi and MSc students to create a simulation tool for forest fires to help prevent propagation. Produced a 3D photorealistic model that can predict effectively fire propagation from parameters such as wind speed, humidity etc.... Used the Unity game engine, C# programming language and state of the art deep learning techniques to build the model",
+  },
+  {
+    title: "CS student council treasurer",
+    company: "Université de Moncton, Moncton",
+    description:
+      "Work with council to prepare the annual budget. Process and record all financial transactions on behalf of the council. Participate in all executive and council discussions and voting",
+  },
+];
+
+export default experience;
