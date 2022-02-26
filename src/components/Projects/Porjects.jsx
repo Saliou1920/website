32ef25bb49a3aa1f756c1f1604cea118897a5fda
Saliou1920 <diopsaliou45@gmail.com>
2022-02-25 20:46:25 -0400

    :art: display 2 cards on medium size screen

--- src/components/Projects/Porjects.jsx
+++ src/components/Projects/Porjects.jsx
@@ -1,35 +1,37 @@
-import { Card, CardGroup, Container } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import "./projects.css";
 import projects from "../../Config/projects";
 export default function Porjects() {
   return (
     <div className="projects" id="projects">
       <Container>
-        <CardGroup>
+        <Row xs={1} md={2} className="g-4">
           {projects.map((project, index) => (
-            <Card id="card" data-aos={project.animation} key={index}>
-              <Card.Img variant="top" src={project.image} />
-              <Card.Body>
-                <Card.Title className="gradient-text">
-                  {project.name}
-                </Card.Title>
-                <Card.Text>{project.description}</Card.Text>
-                <Card.Text>{project.languages}</Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <button className="btn btn-danger">
-                  <a
-                    href={project.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    View
-                  </a>
-                </button>
-              </Card.Footer>
-            </Card>
+            <Col id="card" data-aos={project.animation} key={index}>
+              <Card>
+                <Card.Img variant="top" src={project.image} />
+                <Card.Body>
+                  <Card.Title className="gradient-text">
+                    {project.name}
+                  </Card.Title>
+                  <Card.Text>{project.description}</Card.Text>
+                  <Card.Text>{project.languages}</Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <button className="btn btn-danger">
+                    <a
+                      href={project.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      View
+                    </a>
+                  </button>
+                </Card.Footer>
+              </Card>
+            </Col>
           ))}
-        </CardGroup>
+        </Row>
         <div data-aos="flip-up" className="more-projects">
           <a
             className="btn education-title"
