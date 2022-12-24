4c473f513ef1fb746efa76a1077433813c330e93
Saliou1920 <diopsaliou45@gmail.com>
2022-12-24 08:14:54 -0400

    :hammer: refactoring

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Nav, Container, Button } from "react-bootstrap";
 import "./header.css";
+import About from "../About/About";
 
 export default function Menu() {
   return (
@@ -27,27 +28,7 @@ export default function Menu() {
             </Navbar.Collapse>
           </Container>
         </Navbar>
-        <div className="header-content" id="about">
-          <div className="header-content-inner">
-            <h1 className="gradient-text">Hi I'm Saliou</h1>
-            <p>I am a Software Developer located in New Brunswick.</p>
-            <p>
-              I have a passion for creating beautiful and intuitive user
-              interfaces.
-            </p>
-            <Button
-              href="../../resume.pdf"
-              download
-              variant="danger"
-              style={{ position: "relative" }}
-            >
-              Resume
-            </Button>
-          </div>
-          <div className="header-content-image">
-            <img src="../../saliou.png" alt="me" />
-          </div>
-        </div>
+        <About />
       </Container>
     </div>
   );
