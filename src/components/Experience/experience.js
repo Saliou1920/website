8c8e45e0a26f48685cc4f710d6469af3d0157348
Saliou1920 <diopsaliou45@gmail.com>
2022-12-24 07:57:02 -0400

    :tada: Update job description

--- src/components/Experience/experience.js
+++ src/components/Experience/experience.js
@@ -3,37 +3,37 @@ const experience = [
     title: "Software Developer",
     company: "National Bank of Canada, Montreal",
     description:
-      "Collaborated in a team based, Agile environment to accomplish objectives by deadline. Used React to make a schedule object from the UI to send the json object to DynamoDB. Worked on the migration Camel 3, Java 11 of XMS apps. Used SonarQube, Jenkins for continuous inspection of code quality.",
+      "As a software developer at National Bank Financial Inc. (NBF), I contributed to the development and maintenance of NBDB systems, a division of NBF and a trademark owned by National Bank of Canada (NBC). Working in a collaborative and dynamic team, I utilized Agile methodologies to meet project deadlines and accomplish objectives. I leveraged my skills in React, Angular, and Typescript to enhance the UI and improve the user experience for clients. In addition to my development responsibilities, I also participated in code reviews and PI planning sessions to ensure the quality and efficiency of our work. It's worth noting that NBDB provides order execution only services and makes no investment recommendations; clients are solely responsible for the financial and tax consequences of their investment decisions. In this role, I demonstrated my ability to work effectively in a team environment, my proficiency in various technologies, and my commitment to quality and process improvement.",
   },
   {
     title: "Software Developer",
     company: "Jarvis Consulting, Toronto",
     description:
-      "Developed an MVP for a Linux Cluster Monitoring agent using Agile techniques, a virtual machine provisioned using Google Cloud Platform, bash scripts, git, Docker, and PostgreSQL. Developed apps in Java to mimic Linux grep command and connect to PostgreSQL database. Performed unit testing using the JUnit library. Participated in daily scrum meetings and weekly code reviews",
+      "As a software developer, I successfully delivered an MVP for a Linux Cluster Monitoring agent using Agile techniques. I utilized a variety of tools and technologies, including Google Cloud Platform, bash scripts, git, Docker, and PostgreSQL, to create a virtual machine for the project. In addition, I developed Java applications to mimic the Linux grep command and connect to a PostgreSQL database. I demonstrated my commitment to code quality by conducting unit testing using the JUnit library. I also contributed to the team's success by actively participating in daily scrum meetings and weekly code reviews. Overall, I demonstrated strong problem-solving skills and the ability to work effectively in a collaborative environment.",
   },
   {
     title: "Software Developer Intern",
     company: "Greystone Energy Inc, Moncton",
     description:
-      "Worked with the operation manager to create dashboards using Javascript, SQL, and Power Bi to evaluate employees' performance. Built reports for the operation manager to understand the data analysis steps, enabling him to take important decisions based on performance.",
+      "As an intern at Greystone Energy, I worked closely with the operation manager to create dashboards using Javascript, SQL, and Power Bi to evaluate employee performance. I utilized my skills in data analysis and visualization to build reports for the operation manager, providing him with the insights he needed to make informed decisions about employee performance. In this role, I demonstrated my ability to work effectively with a manager and my technical skills in Javascript, SQL, and Power Bi. I also showed my dedication to supporting the organization's decision-making process through the use of data analysis.",
   },
   {
     title: "C++ programming Tutor",
     company: "Université de Moncton, Moncton",
     description:
-      "Worked closely with the professor and a few other computer science students to assist junior and sophomore students with C++ programming. Successfully, analyzed and debugged code written by various students and clarified the new concepts introduced in the course.",
+      "As a computer science student, I had the opportunity to work closely with a professor and other students to assist junior and sophomore students with their C++ programming coursework. I successfully analyzed and debugged code written by a diverse group of students, and used my excellent communication skills to clarify complex concepts and troubleshoot any issues they encountered. My dedication to helping others succeed and my strong technical skills allowed me to effectively assist my peers and contribute to the overall success of the program. Through this experience, I gained valuable hands-on experience in coding and instruction, and developed my skills in teamwork, problem-solving, and leadership.",
   },
   {
     title: "AI Research Assistant",
     company: "Perception, Robotics and intelligent Machines (PRIME), Moncton",
     description:
-      "Worked with Professor Akhloufi and MSc students to create a simulation tool for forest fires to help prevent propagation. Produced a 3D photorealistic model that can predict effectively fire propagation from parameters such as wind speed, humidity etc.... Used the Unity game engine, C# programming language and state of the art deep learning techniques to build the model",
+      "As an intern in the Computer Science department, I had the opportunity to work with Professor Akhloufi and MSc students on a project to create a simulation tool for forest fires. My responsibilities included creating a 3D photorealistic model using the Unity game engine and C# programming language, as well as implementing state-of-the-art deep learning techniques to accurately predict fire propagation based on parameters such as wind speed and humidity. I successfully developed a tool that has the potential to help prevent the spread of forest fires, and gained valuable experience in programming, simulation, and deep learning. This internship allowed me to utilize my technical skills and problem-solving abilities to make a meaningful contribution to a real-world problem.",
   },
   {
     title: "CS student council treasurer",
     company: "Université de Moncton, Moncton",
     description:
-      "Work with council to prepare the annual budget. Process and record all financial transactions on behalf of the council. Participate in all executive and council discussions and voting",
+      "As a student member of the council, I played a key role in the preparation of the annual budget and the management of financial transactions. I worked closely with the council to process and record all financial activity on behalf of the organization, and actively participated in executive and council discussions and voting. This experience allowed me to develop my skills in financial management, decision-making, and teamwork, and gave me a firsthand understanding of the inner workings of a council. I demonstrated my ability to handle sensitive financial information with care and attention to detail, and contributed to the smooth operation and success of the council.",
   },
 ];
 
